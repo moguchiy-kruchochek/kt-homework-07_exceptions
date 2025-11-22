04fcf5bca7ed7eb4cd513d8a8f3cd51de5c5d914
Jaregg <starayaglina@gmail.com>
2025-11-22 14:50:51 +0700

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,80 @@
+package ru.netology
+
+
+data class Likes(
+    val count: Int = 10,
+    val userLikes: Boolean = true,
+    val canLike: Boolean = true,
+    val canRepost: Boolean = false
+)
+
+data class Post(
+    val id: Int = 0,
+    val date: String,
+    val text: String,
+    val friendsOnly: Boolean,
+    val copyright: String,
+    val isPinned: Boolean,
+    val isFavorite: Boolean,
+    val marksAsAds: Boolean,
+    val likes: Likes,
+)
+
+
+class WallService {
+    private var posts = emptyArray<Post>()
+    private var unicId = 1
+
+    fun add(post: Post): Post {
+        val postWithUnicId = post.copy(id = unicId)
+        posts += postWithUnicId
+        unicId += 1
+
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, postWithSameId) in posts.withIndex()) {
+            if (postWithSameId.id == post.id) {
+                posts[index] = post
+                return true
+            }
+        }
+        return false
+    }
+}
+
+
+fun main() {
+    val service = WallService()
+    val likes = Likes()
+
+
+    val post = Post(
+        0,
+        "11.11.11",
+        "text",
+        friendsOnly = true,
+        "author",
+        isPinned = true,
+        isFavorite = true,
+        marksAsAds = true,
+        likes,
+    )
+    service.add(post)
+    service.update(
+        Post(
+            1,
+            "03.04.25",
+            "Important text",
+            false,
+            "me",
+            true,
+            false,
+            false,
+            likes,
+
+        )
+    )
+    println(post)
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,154 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import ru.netology.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        val service = WallService()
+        val likes = Likes()
+
+
+        val post = Post(
+            3,
+            "31.10.21",
+            "text33",
+            true,
+            "NEauthor",
+            true,
+            true,
+            false,
+            likes,
+        )
+
+        val result = service.add(post)
+        assertEquals(1, result.id)
+    }
+
+    @Test
+    fun updateExistingWithTrueResult() {
+        val service = WallService()
+        val likes = Likes()
+
+        service.add(
+            Post(
+                0,
+                "11.11.20",
+                "text51",
+                true,
+                "authorr",
+                true,
+                false,
+                true,
+                likes,
+            )
+        )
+        service.add(
+            Post(
+                0,
+                "23.12.21",
+                "super text",
+                false,
+                "corp",
+                true,
+                true,
+                false,
+                likes,
+            )
+        )
+        service.add(
+            Post(
+                0,
+                "03.03.25",
+                "important",
+                true,
+                "mine",
+                true,
+                false,
+                false,
+                likes,
+            )
+        )
+
+        val update = Post(
+            3,
+            "03.04.25",
+            "IMPORTANT!",
+            true,
+            "me",
+            true,
+            true,
+            true,
+            likes,
+        )
+
+        val result = service.update(update)
+
+        assertTrue("The Post updated!", result)
+    }
+
+    @Test
+    fun updateWithFalseResult() {
+        val service = WallService()
+        val likes = Likes()
+
+        service.add(
+            Post(
+                0,
+                "11.11.20",
+                "text51",
+                true,
+                "authorr",
+                true,
+                false,
+                true,
+                likes,
+            )
+
+        )
+        service.add(
+            Post(
+                0,
+                "23.12.21",
+                "super text",
+                false,
+                "corp",
+                false,
+                true,
+                false,
+                likes,
+            )
+        )
+        service.add(
+            Post(
+                0,
+                "03.03.25",
+                "important",
+                false,
+                "mine",
+                true,
+                false,
+                false,
+                likes,
+            )
+        )
+
+        val update = Post(
+            6,
+            "03.04.25",
+            "IMPORTANT!",
+            true,
+            "me",
+            true,
+            true,
+            true,
+            likes,
+        )
+
+        val result = service.update(update)
+
+        assertFalse("No Posts with such ID :(", result)
+    }
+}
